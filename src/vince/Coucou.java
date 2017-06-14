e45b448286f3c59df9447b405c32d0ccc1ae4497
Gogs <gogs@fake.local>
2017-06-14 15:01:32 +0200

    coucou
    Signed-off-by: Gogs <gogs@fake.local>

---
+++ src/vince/Coucou.java
@@ -0,0 +1,10 @@
+package vince;
+
+public class Coucou {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/vince/Hey.java
@@ -0,0 +1,5 @@
+package vince;
+
+public class Hey {
+
+}

31fd6150a18c3074cb7bf3e29efa6f208d0a7742
Gogs <gogs@fake.local>
2017-06-14 15:58:44 +0200

    bla

--- src/vince/Coucou.java
+++ src/vince/Coucou.java
@@ -4,11 +4,7 @@ public class Coucou {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("coucou");
-=======
->>>>>>> refs/remotes/origin/master
-
 	}
 
 }
